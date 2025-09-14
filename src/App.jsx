4fd6ef484c47eb96e8a068d49ab9c3acaa4400ae
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-14 15:01:41 -0300

    qweqwe

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,6 @@ import Inicio from "./assets/componentes/Inicio";
 import Resmas from "./assets/componentes/Resmas";
 
 function App() {
-  // Test de conexión con el backend en Railway
-  useEffect(() => {
-    const API_URL = import.meta.env.VITE_API_URL;
-
-    fetch(`${API_URL}/ping`)
-      .then((res) => res.text())
-      .then((data) => console.log("✅ Conexión con backend:", data))
-      .catch((err) => console.error("❌ Error al conectar con Railway:", err));
-  }, []);
 
   return (
     <>
