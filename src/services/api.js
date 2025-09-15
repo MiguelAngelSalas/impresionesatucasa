1353e7dd1ce8f297c474925362cf8276ee0a9944
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-14 22:12:12 -0300

    asdasdasdadsasd

---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+export const subirArchivo = async ({ archivo, tipoPapel, nombreCliente }) => {
+  const formData = new FormData();
+  formData.append("file", archivo);
+  formData.append("paperType", tipoPapel);
+  formData.append("clientName", nombreCliente);
+
+  try {
+    const respuesta = await fetch(`${API_URL}/upload`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (respuesta.ok) {
+      const { pedido } = await respuesta.json();
+      return {
+        mensaje: "✅ Pedido recibido correctamente",
+        pedido,
+      };
+    } else {
+      const error = await respuesta.json();
+      return { mensaje: error.message || "Error al subir el archivo." };
+    }
+  } catch (error) {
+    console.error("Error al conectar con el servidor:", error);
+    return { mensaje: "Error de conexión con el servidor." };
+  }
+};
