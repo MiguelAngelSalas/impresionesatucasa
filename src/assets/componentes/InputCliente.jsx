b6d094a7b1a77a1d942db2013060c486b5c8a482
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-16 08:50:06 -0300

    Update InputCliente.jsx
    Opcional pero importante

--- src/assets/componentes/InputCliente.jsx
+++ src/assets/componentes/InputCliente.jsx
@@ -4,12 +4,13 @@ const InputCliente = ({ value, onChange }) => {
   return (
     <input
       type="text"
-      placeholder="Tu nombre o contacto (opcional)"
+      placeholder="Tu nombre (opcional pero importante)"
       value={value}
       onChange={(e) => onChange(e.target.value)}
       className="w-full border border-gray-300 rounded-lg px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-violet-400"
     />
   );
 };
+
 
 export default InputCliente;
