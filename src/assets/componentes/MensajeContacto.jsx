066cb58663cb4b662cab2db6678131f1dc867694
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-22 19:10:44 -0300

    Create MensajeContacto.jsx
    Modularizacion

---
+++ src/assets/componentes/MensajeContacto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MensajeContacto = () => (
+  <div className="bg-green-100 border border-green-300 text-green-800 text-sm font-medium px-4 py-2 rounded-lg shadow-sm text-center mt-4">
+    📞 En breve nos comunicaremos por WhatsApp para confirmar tu pedido
+  </div>
+);
+
+export default MensajeContacto;
