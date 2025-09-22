a447b4c9a90ac7c910b0dd5e2f0b5552e0645eaa
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-22 19:20:36 -0300

    Create CarcularDescuento.jsx
    Modularizacion

---
+++ src/utils/CarcularDescuento.jsx
@@ -0,0 +1,7 @@
+export const calcularDescuento = (paginas) => {
+  if (paginas > 50) return 0.3;
+  if (paginas > 30) return 0.2;
+  if (paginas > 20) return 0.15;
+  if (paginas > 10) return 0.1;
+  return 0;
+};
