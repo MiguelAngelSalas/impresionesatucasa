d988993717491aae0b8ae428003ef1473cd95182
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-10-24 14:53:23 -0300

    Update api.js

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,4 +1,4 @@
-const API_URL = "import.meta.env.VITE_API_URL";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const subirArchivo = async ({ archivo, tipoPapel, nombreCliente, telefonoCliente, paginas }) => {
   const formData = new FormData();
