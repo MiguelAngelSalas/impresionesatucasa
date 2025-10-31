95f349a1af00425c236e6dd356fb9d3864ae0a91
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-10-31 06:53:35 -0300

    Update FormularioEnvio.jsx

--- src/componentes/FormularioEnvio.jsx
+++ src/componentes/FormularioEnvio.jsx
@@ -4,7 +4,6 @@ import MensajeEstado from "./MensajeEstado";
 
 const FormularioEnvio = ({ manejarEnvio, manejarAgregarAlCarrito, estado }) => (
   <>
-    <BotonEnviar onClick={manejarEnvio} />
     <button
       onClick={manejarAgregarAlCarrito}
       className="w-full bg-violet-600 hover:bg-violet-700 text-white font-semibold py-2 px-4 rounded-md shadow-sm transition"
